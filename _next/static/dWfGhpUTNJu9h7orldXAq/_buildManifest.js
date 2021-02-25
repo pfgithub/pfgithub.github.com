ef10871bab85689c6346bfceab029b03c7ae980e
pfgithub <omegariisthebest@gmail.com>
2021-02-25 08:30:00 +0000

    Deploying to gh-pages from @ pfgithub/pfgithub.github.com@b59d8853e276fabfc415646782b6add493b107d0 🚀

---
+++ _next/static/dWfGhpUTNJu9h7orldXAq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-536465604a920d94aedd.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a2fc0a9cb640f96e5e42.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
